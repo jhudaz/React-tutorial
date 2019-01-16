95a1b58c0afeb92350f03a3d69d7dd858ee73bf3
jaime andres <jandres.vr@hotmail.com>
2019-01-16 18:28:27 -0500

    Update board.js
    Passing the functions to the App Component through the properties

--- src/board.js
+++ src/board.js
@@ -2,31 +2,50 @@ import React, { Component } from 'react';
 import App from './App';
 import './App.css';
 
-class Board extends Component{
-    constructor(props){
+class Board extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            comments:[
+        this.state = {
+            comments: [
                 "Hola mundo!",
                 "Aún estoy aqui...",
-                "Ya me voy!",
-                "Volvi!"
+                "Ya me voy!"
             ]
         }
+        this.delete = this.delete.bind(this);
+        this.update = this.update.bind(this);
     }
-
-    render(){
-        return(
+    delete(i) {
+        console.log("The comment deleted was the #", i);
+        var newArr = this.state.comments;
+        newArr.slice(i, 1);//slice function take two parameters that are the index where it start and how many elements should delete
+        this.setState({ comments: newArr });
+    }
+    update(nextText, i) {
+        console.log("The comment was updated!");
+        var newArr = this.state.comments;
+        newArr[i] = nextText;
+        this.setState({ comments: newArr });
+    }
+    //this function it will put the  text using the elements inside of comments state  into the App component
+    eachComment(text, i) {
+        return (
+            //we are going to send a function to the other component through a property with a any nick that we want to use
+            <App key={i} index={i} updateComment={this.update} deleteComment={this.delete} >
+                {text}
+            </App>
+        );
+    }
+    //here we only map the elements inside comments state 
+    render() {
+        return (
             <div>
-               {this.state.comments.map(function(text,i){
-                   return (<App>{text}</App>);
-               })}
+                {this.state.comments.map(this.eachComment)}
             </div>
         )
     }
 
 
 }
-
 
 export default Board;
